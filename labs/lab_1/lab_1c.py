0fe04b14d7ba61c13d3c68db366011e7ae45e5b5
Adit Gupta <guptaditz@gmail.com>
2026-03-13 14:30:22 -0700

    Added tests for maxsubarray

--- labs/lab_1/lab_1c.py
+++ labs/lab_1/lab_1c.py
@@ -19,19 +19,20 @@ def max_subarray_sum(nums: list[int]) -> int:
     Returns:
         int: The maximum sum of any contiguous subarray.
     """
+    if not nums:
+        raise ValueError("Array is Empty")
 
     max_current = max_global = nums[0]
     
     for num in nums:
         max_current = max(num, max_current + num)
-        if max_current < max_global:
+        if max_current > max_global:
             max_global = max_current
             
     return max_global
 
 # Example usage:
 def main():
-    nums = [-2,1,-3,4,-1,2,1,-5,4]
     result = max_subarray_sum(nums)
     print(f"Maximum subarray sum: {result}")
 
